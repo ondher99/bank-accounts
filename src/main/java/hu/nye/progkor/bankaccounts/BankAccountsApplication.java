b51506620645e503b48cd0cecd956538afa7b97a
Botond Andor Heringh <heringh.botond@gmail.com>
2023-05-20 20:21:57 +0200

    First commit

---
+++ src/main/java/hu/nye/progkor/bankaccounts/BankAccountsApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.bankaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/progkor/bankaccounts/BankAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.bankaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
